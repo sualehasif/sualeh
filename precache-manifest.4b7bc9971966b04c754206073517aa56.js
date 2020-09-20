5b55aaec24251684600533a187a11846ebf56759
sualehasif996 <sualehasif996@gmail.com>
2020-09-19 22:38:19 -0400

    Updates

---
+++ precache-manifest.4b7bc9971966b04c754206073517aa56.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "906e00cbb02e6017e02481788758ad57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3503a05f561748710322",
+    "url": "/static/css/2.9ef65634.chunk.css"
+  },
+  {
+    "revision": "3503a05f561748710322",
+    "url": "/static/js/2.9a74da1a.chunk.js"
+  },
+  {
+    "revision": "a5da5d53dfba093fa63375799ffeddd6",
+    "url": "/static/js/2.9a74da1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17ddf5943359dee71b76",
+    "url": "/static/js/main.d948978e.chunk.js"
+  },
+  {
+    "revision": "6388accb66305a34d5bd",
+    "url": "/static/js/runtime-main.a6427217.js"
+  },
+  {
+    "revision": "e5baeb7575615cab700936e2bf922ae5",
+    "url": "/static/media/cupcake.e5baeb75.jpg"
+  },
+  {
+    "revision": "72c8ac0a88ed09b50bb15a9abca9b9cf",
+    "url": "/static/media/cupcake2.72c8ac0a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sualehasif.me/precache-manifest.9ffc90f8cd450a1adf10552c919caa4a.js"
+  "/precache-manifest.4b7bc9971966b04c754206073517aa56.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sualehasif.me/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
